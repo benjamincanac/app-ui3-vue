980bc73ddb77f6077cd933251dbaab04934bf558
Benjamin Canac <canacb1@gmail.com>
2025-02-17 14:26:36 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
+import './assets/css/main.css'
+
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import nuxtUI from '@nuxt/ui/vue-plugin'
+import ui from '@nuxt/ui/vue-plugin'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createRouter({
-  routes: [{ path: '/', component: () => import('./pages/Home.vue') }],
+  routes: [{ path: '/', component: () => import('./pages/index.vue') }],
   history: createWebHistory()
 }))
 
 // allow Nuxt UI to register vue plugins, like dark mode support + NuxtIcon
-app.use(nuxtUI)
+app.use(ui)
 
 app.mount('#app')
